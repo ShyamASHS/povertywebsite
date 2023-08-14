0bae5b0b9f99bc5761b77532885ae1325de80efa
ShyamASHS <112436001+ShyamASHS@users.noreply.github.com>
2023-08-14 14:25:07 +1200

    Update script.js

--- Assessment/images/index.html/script.js
+++ Assessment/images/index.html/script.js
@@ -5,3 +5,30 @@ document.addEventListener("DOMContentLoaded", function() {
       login.classList.toggle('active');
     };
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".gallery-slider", {
+      grabCursor: true,
+      loop: true,
+      centeredSlides: true,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        700: {
+          slidesPerView: 2,
+        },
+      },
+    });
+  });
+ 
+  
+  
+  
+  
+  
